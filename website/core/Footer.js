93eff201a670a407c716ca8cabd121a431defbf0
SanLi <2689170096@qq.com>
2019-05-12 15:30:27 +0800

    init

---
+++ website/core/Footer.js
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+class Footer extends React.Component {
+    docUrl(doc, language) {
+        const baseUrl = this.props.config.baseUrl;
+        const docsUrl = this.props.config.docsUrl;
+        const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+        const langPart = `${language ? `${language}/` : ''}`;
+        return `${baseUrl}${docsPart}${langPart}${doc}`;
+    }
+
+    pageUrl(doc, language) {
+        const baseUrl = this.props.config.baseUrl;
+        return baseUrl + (language ? `${language}/` : '') + doc;
+    }
+
+    render() {
+        return (
+            <footer className="nav-footer" id="footer">
+                <section className="sitemap">
+                    <a href={this.props.config.baseUrl} className="nav-home">
+                        {this.props.config.footerIcon && (
+                            <img
+                                src={this.props.config.baseUrl + this.props.config.footerIcon}
+                                alt={this.props.config.title}
+                                width="66"
+                                height="58"
+                            />
+                        )}
+                    </a>
+                    <div>
+                        <h5>导航</h5>
+                        <a href={this.docUrl('doc2.html', this.props.language)}>
+                            使用文档
+                        </a>
+                        <a href={this.docUrl('doc2.html', this.props.language)}>
+                            在线演示
+                        </a>
+                        <a href={this.docUrl('doc3.html', this.props.language)}>
+                            技术支持
+                        </a>
+                    </div>
+                    <div>
+                        <h5>社区</h5>
+                        <a href={this.pageUrl('users.html', this.props.language)}>
+                            使用公司
+                        </a>
+                        <a href="https://discordapp.com/">微信群</a>
+                        <a href="https://discordapp.com/">QQ群</a>
+                        <a
+                            href="https://twitter.com/"
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            钉钉群
+                        </a>
+                    </div>
+                    <div>
+                        <h5>更多</h5>
+                        <a href={`${this.props.config.baseUrl}blog`}>更新日志</a>
+                        <a href="https://github.com/">GitHub</a>
+                        <a
+                            className="github-button"
+                            href={this.props.config.repoUrl}
+                            data-icon="octicon-star"
+                            data-count-href="/facebook/docusaurus/stargazers"
+                            data-show-count="true"
+                            data-count-aria-label="# stargazers on GitHub"
+                            aria-label="Star this project on GitHub">
+                            Star
+                        </a>
+                    </div>
+                </section>
+
+                <a
+                    href="https://opensource.facebook.com/"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="fbOpenSource">
+                    <img
+                        src={`${this.props.config.baseUrl}img/oss_logo.png`}
+                        alt="SmallBun"
+                        width="170"
+                        height="45"
+                    />
+                </a>
+                <section className="copyright">{this.props.config.copyright}</section>
+            </footer>
+        );
+    }
+}
+
+module.exports = Footer;
---
+++ website/siteConfig.js
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// 尽可能查看 https://docusaurus.io/docs/site-config
+// 站点配置选项。
+
+// 使用您的项目为用户页面的项目/组织列表。
+const users = [
+    {
+        caption: 'User1',
+        // 您需要在baseUrl前面添加图像路径
+        // 如果它不是'/'，例如：'/ test-site/img/image.jpg'。
+        image: '/img/undraw_open_source.svg',
+        infoLink: 'https://www.facebook.com',
+        pinned: true,
+    },
+];
+
+const siteConfig = {
+    //algolia 文档搜索
+    algolia: {
+        apiKey: "my-search-only-api-key-1234",
+        indexName: "my-index-name"
+    },
+    title: 'SmallBun', // 您网站的标题
+    tagline: '企业级应用程序开发脚手架',
+    url: 'https://pingfangushi.github.io', // 网站网址
+    baseUrl: 'smallbun', // 项目的基本URL */
+
+    // 用于发布等
+    projectName: 'smallbun-website',
+    organizationName: 'pingfangushi',
+
+
+    // 对于顶部导航栏中没有标题链接 - > headerLinks：[]，
+    headerLinks: [
+        {doc: "项目概述", label: "使用文档"},
+        {href: "http://118.25.232.156:9090/login", label: "在线演示"},
+        {doc: "技术支持", label: "技术支持"},
+        {blog: true, label: "更新日志"},
+        {href: "https://github.com/leshalv/smallbun", label: "GitHub"},
+        {search: true},
+    ],
+
+    // 如果您在上面设置了用户，请在此处添加：
+    users,
+
+    /* 页眉/页脚的图像路径 */
+    /*headerIcon: 'img/favicon.ico',*/
+    /*footerIcon: 'img/favicon.ico',*/
+    /*favicon: 'img/favicon.ico',*/
+
+    /* 网站的颜色 */
+    colors: {
+        primaryColor: '#1890ff',
+        secondaryColor: '#096dd9',
+    },
+
+    // 此版权信息用于/core/Footer.js和博客RSS / Atom供稿
+    copyright: `Copyright © ${new Date().getFullYear()} SmallBun 企业级开发脚手架`,
+
+    highlight: {
+        // Highlight.js主题用于代码块中的语法突出显示。
+        theme: 'default',
+    },
+
+    // 在此处添加将放置在<script>标记中的自定义脚本。
+    scripts: ['https://buttons.github.io/buttons.js'],
+
+    // 在当前文档页面的页面导航上。
+    onPageNav: 'separate',
+    // 路径没有.html扩展名。
+    cleanUrl: true,
+
+    // 打开Graph和Twitter卡图片。
+    ogImage: 'img/undraw_online.svg',
+    twitterImage: 'img/undraw_tweetstorm.svg',
+
+    // 显示文档的最后一个贡献者的名字。
+    enableUpdateBy: true,
+
+    // 显示文档的上次更新时间
+    enableUpdateTime: true,/**/
+};
+
+module.exports = siteConfig;
